5979cd68a61c32688a7d10db3f6ed42e6a428caf
Newton Migosi <newtonmigosi@gmail.com>
2018-04-17 11:45:36 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 function forLoop(arr) {
   var s = '';
-  for (let i = 1; i < 25; i++) {
+  for (let i = 1; i < 26; i++) {
     if (i > 1) {s = 's'}
     var str = `I am ${i} strange loop${s}.`;
     arr.push(str);
